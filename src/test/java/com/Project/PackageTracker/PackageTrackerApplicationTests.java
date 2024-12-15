74610468508a5a27837fc78567c58da2beba15d2
Jose <josedanielduran222@gmail.com>
2024-12-15 15:04:11 +0100

    Initial commit

---
+++ src/test/java/com/Project/PackageTracker/PackageTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.PackageTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
