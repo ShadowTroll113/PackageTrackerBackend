33a1b547b6f0ca3b6c01d08ece8729a0310d55f1
Jose <josedanielduran222@gmail.com>
2025-06-22 22:30:50 +0200

    move to the right folder, deleted user generating code for initial commits

--- src/main/java/com/Project/PackageTracker/config/WebConfig.java
+++ src/main/java/com/Project/PackageTracker/config/WebConfig.java
@@ -1,6 +1,5 @@
-package com.Project.PackageTracker;
+package com.Project.PackageTracker.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
